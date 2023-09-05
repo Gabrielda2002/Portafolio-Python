4a890793e9ccbbd32924d5646dfd4e2f224ad0b2
Gabriel Duarte <duartearias2002@gmail.com>
2023-09-05 08:33:15 -0500

    agregando ejercicios

---
+++ Ejercicio1/Ejemplo1.py
@@ -0,0 +1,13 @@
+print("Ingrese el numero que desea validar")
+num=int(input())
+
+if num == 0:
+    print("El numero es Neutro")
+elif num<0:
+    print("El numero es negativo")
+else:
+    print("El numero es positivo")
+
+
+# print(f"El numero es un numero: ")
+    
