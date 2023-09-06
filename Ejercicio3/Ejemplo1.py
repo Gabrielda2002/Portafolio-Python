1b3458467118ba9e72480f09d89c7cf686668d45
Gabriel Duarte <duartearias2002@gmail.com>
2023-09-06 18:32:00 -0500

    ejercicio 1

---
+++ Ejercicio3/Ejemplo1.py
@@ -0,0 +1,18 @@
+print("digite un numero de 1 a 7")
+num=int(input())
+
+match num:
+    case 1:
+        print("Lunes")
+    case 2:
+        print("Martes")
+    case 3:
+        print("Miercoles")
+    case 4:
+        print("Jueves")
+    case 5:
+        print("Viernes")
+    case 6:
+        print("Sabado")
+    case 7:
+        print("Domingo")
