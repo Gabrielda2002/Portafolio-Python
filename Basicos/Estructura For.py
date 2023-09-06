205ae63e9e761df56386a176e1b02f1aa071735a
Gabriel Duarte <duartearias2002@gmail.com>
2023-09-06 12:34:53 -0500

    arreglando error for en Bascios

--- Basicos/Estructura For.py
+++ Basicos/Estructura For.py
@@ -1,7 +1,6 @@
 suma=0 #se crea la variable global
 print("digite la cantidad de numeros para sumar")
 cantidad = int(input())
-
 for i in range(cantidad):
     print("Digite el numero" + str(i+1)+": ")
     numero=int(input())
