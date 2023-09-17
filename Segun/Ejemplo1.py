d289e76a5686a346d08e204d5deda7c5f754dadb
Gabriel Duarte <duartearias2002@gmail.com>
2023-09-17 15:49:12 -0500

    complementando ejercicio1 de segun

--- Segun/Ejemplo1.py
+++ Segun/Ejemplo1.py
@@ -1,18 +1,29 @@
-print("digite un numero de 1 a 7")
-num=int(input())
+while True:
 
-match num:
-    case 1:
-        print("Lunes")
-    case 2:
-        print("Martes")
-    case 3:
-        print("Miercoles")
-    case 4:
-        print("Jueves")
-    case 5:
-        print("Viernes")
-    case 6:
-        print("Sabado")
-    case 7:
-        print("Domingo")
+    print("digite un numero de 1 a 7")
+    num=int(input())
+
+    match num:
+        case 1:
+            print("Lunes")
+            break
+        case 2:
+            print("Martes")
+            break
+        case 3:
+            print("Miercoles")
+            break
+        case 4:
+            print("Jueves")
+            break
+        case 5:
+            print("Viernes")
+            break
+        case 6:
+            print("Sabado")
+            break
+        case 7:
+            print("Domingo")
+            break
+        case defecto:
+            print("Error! Ingrese una opcion valida")
